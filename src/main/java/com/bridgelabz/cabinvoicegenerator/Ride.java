14f3b81ee55fef48846b8c6d18d2548745900a6e
shephali09 <hiremathshephali9@gmail.com>
2023-05-04 10:49:35 +0530

    Add: The invoice generator should now take in multiple rides, and calculate the aggregate total for all.

---
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/Ride.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+public class Ride {
+
+	public final int time;
+	public double distance;
+
+	public Ride(double distance, int time) {
+		this.distance = distance;
+		this.time = time;
+	}
+}
--- src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorTest.java
@@ -12,18 +12,26 @@ public class InvoiceGeneratorTest {
 		double distance = 2.0;
 		int time = 5;
 		double fare = invoiceGenerator.calculateFare(distance, time);
-		Assert.assertEquals(25,fare, 0.0);
-		
+		Assert.assertEquals(25, fare, 0.0);
 	}
-	
+
 	@Test
 	public void givenLessDistanceAndTime() {
 		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 		double distance = 0.1;
 		int time = 1;
 		double fare = invoiceGenerator.calculateFare(distance, time);
-		Assert.assertEquals(5,fare, 0.0);
-		
+		Assert.assertEquals(5, fare, 0.0);
+	}
+
+	@Test
+	public void givenMultipleRides() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1)
+
+		};
+		double fare = invoiceGenerator.calculateFare(rides);
+		Assert.assertEquals(30, fare, 0.0);
 	}
 
 }
